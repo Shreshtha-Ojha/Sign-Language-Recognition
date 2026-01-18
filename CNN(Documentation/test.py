dd41e1073435a2b3aaf8fb7ee5e5e2f6ea37796e
Shreshtha Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2026-01-18 20:39:20 +0530

    Update test.py

--- CNN(Documentation/test.py
+++ CNN(Documentation/test.py
@@ -1,7 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 from cvzone.ClassificationModule import Classifier
-import numpy as n
+import numpy as np
 import math
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
